06f18d5856d0d9bc9b6ccbd7318868fd2556cf7b
h0uter <houterm@gmail.com>
2018-11-27 09:26:28 +0100

    uml planning

---
+++ Design & UML/Goals.md
@@ -0,0 +1,5 @@
+# **Goals**
+
+* multiRoom
+* design to add fighters
+* 
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ErrorMapper } from "utils/ErrorMapper";
 export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current game tick is ${Game.time}`);
 
+
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
     if (!(name in Game.creeps)) {
